3f5de1dbac44722434d6efad0e9d8bacb2d5d3d7
Amandine SAHL <amandine.sahl@cevennes-parcnational.fr>
2022-05-20 15:17:56 +0200

    Set absolute path

--- etalab/push_to_datagouv.sh
+++ etalab/push_to_datagouv.sh
@@ -1,18 +1,20 @@
-. settings.ini
+
+CURRENT_DIR=`dirname $0`
+. ${CURRENT_DIR}/settings.ini
 
 API_URL='https://www.data.gouv.fr/api/1'
-EXPORT_PATH="../.."
+
 FILE_NAME="itineraires_rando_export.json"
 
 
 # Publication sur etalab
 
-if [[ -f "${EXPORT_PATH}/$FILE_NAME" ]]; then
+if [[ -f "${CURRENT_DIR}/../$FILE_NAME" ]]; then
   curl -H "Accept:application/json" \
       -H "X-Api-Key:$API_KEY" \
-      -F "file=@${EXPORT_PATH}/${FILE_NAME}" \
+      -F "file=@${CURRENT_DIR}/../${FILE_NAME}" \
       -X POST $API_URL/datasets/$DATASET/resources/$RESOURCE/upload/
 else
-  echo "${EXPORT_PATH}/${FILE_NAME} does not exist."
+  echo "${CURRENT_DIR}/../${FILE_NAME} does not exist."
 fi
 
