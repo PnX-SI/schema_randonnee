b378660cf32fb3db6c065acd5bc65240d1888802
Amandine SAHL <amandine.sahl@cevennes-parcnational.fr>
2022-05-31 10:03:10 +0200

    Add shebang and POSIX synthax

--- data_gouv_fr/push_to_datagouv.sh
+++ data_gouv_fr/push_to_datagouv.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 CURRENT_DIR=`dirname $0`
 . ${CURRENT_DIR}/settings.ini
 
@@ -8,7 +10,7 @@ FILE_NAME="itineraires_rando_export.json"
 
 # Publication sur data.gouv.fr
 
-if [[ -f "${CURRENT_DIR}/../$FILE_NAME" ]]; then
+if [ -f "${CURRENT_DIR}/../$FILE_NAME" ]; then
   curl -H "Accept:application/json" \
       -H "X-Api-Key:$API_KEY" \
       -F "file=@${CURRENT_DIR}/../${FILE_NAME}" \
