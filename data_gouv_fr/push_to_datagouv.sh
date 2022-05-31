d951bc7e8fa5c80f83497e4bc26c94351b2cd8c4
Amandine SAHL <amandine.sahl@cevennes-parcnational.fr>
2022-05-31 14:04:54 +0200

    Add optional parameter: change file name

--- data_gouv_fr/push_to_datagouv.sh
+++ data_gouv_fr/push_to_datagouv.sh
@@ -7,14 +7,23 @@ API_URL='https://www.data.gouv.fr/api/1'
 
 FILE_NAME="itineraires_rando_export.json"
 
+if [ -z ${EXPORT_FILE_NAME+x} ];
+then
+  EXPORT_FILE_NAME="itineraires_rando_export"
+fi
+FULL_FILE_PATH=${CURRENT_DIR}/../${EXPORT_FILE_NAME}.json
+
+mv ${CURRENT_DIR}/../${FILE_NAME} ${FULL_FILE_PATH}
 
 # Publication sur data.gouv.fr
 
-if [ -f "${CURRENT_DIR}/../$FILE_NAME" ]; then
+
+
+if [ -f "${FULL_FILE_PATH}" ]; then
   curl -H "Accept:application/json" \
       -H "X-Api-Key:$API_KEY" \
-      -F "file=@${CURRENT_DIR}/../${FILE_NAME}" \
+      -F "file=@${FULL_FILE_PATH}" \
       -X POST $API_URL/datasets/$DATASET/resources/$RESOURCE/upload/
 else
-  echo "${CURRENT_DIR}/../${FILE_NAME} does not exist."
+  echo "${FULL_FILE_PATH} does not exist."
 fi
