90c080f5e7c6300fb95f45e4a2a5955a87487c49
Idrissa Djepa Creutz <idrizza@hotmail.fr>
2023-08-11 11:13:18 +0200

    Optimization of imports and abandon of soon-to-be deprecated jsonschema function

--- tools/2_validate_data/validate_data_with_jsonschema.py
+++ tools/2_validate_data/validate_data_with_jsonschema.py
@@ -1,15 +1,15 @@
-import sys
-import json
-import jsonschema
+from sys import exit
+from json import load
+from jsonschema.validators import Draft7Validator
 
 
 with open('../../schema.json', 'r') as schema_file:
-    schema = json.load(schema_file)
+    schema = load(schema_file)
 
 with open('../generated_data/itineraires_rando.json', 'r') as instance_file:
-    instance = json.load(instance_file)
+    instance = load(instance_file)
 
-validator = jsonschema.Draft7Validator(schema)
+validator = Draft7Validator(schema)
 
 errors = sorted(validator.iter_errors(instance), key=lambda e: e.path)
 
@@ -18,6 +18,6 @@ if len(errors) > 0:
     for error in errors:
         nom_itineraire = (instance["features"][error.path[1]]["properties"]["nom_itineraire"])
         print(f"[{error.path[1]}], {error.path[3]}: {error.message} - ({nom_itineraire})")
-    sys.exit(1)
+    exit(1)
 else:
     print("Fichier de données valide !")
